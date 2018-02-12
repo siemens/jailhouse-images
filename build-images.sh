c87cf8eec8bf3e4f88561da399b5ef78a89a0755
Jan Kiszka <jan.kiszka@siemens.com>
2018-02-12 08:10:21 +0100

    build-images: Add --shell switch for interactive mode
    Helps while developing: Call build-images --shell, you will fall into a
    kas shell in the docker container so that bitbake can be called
    manually.
    
    Rename the repo mount point at this chance to the official project name.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- build-images.sh
+++ build-images.sh
@@ -10,13 +10,36 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 
+usage()
+{
+	echo "Usage: $0 [OPTIONS]"
+	echo -e "\nOptions:"
+	echo -e "--shell\t\tDrop into a shell to issue bitbake commands" \
+		"manually."
+	exit 1
+}
+
+CMD="build"
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+	--shell)
+		CMD="shell"
+		shift 1
+		;;
+	*)
+		usage
+		;;
+	esac
+done
+
 mkdir -p out
-docker run -v $(pwd):/isar-jailhouse:ro -v $(pwd)/out:/out:rw \
-	   -e USER_ID=$(id -u) --rm -t -i \
+docker run -v $(pwd):/jailhouse-images:ro -v $(pwd)/out:/out:rw \
+	   -e USER_ID=$(id -u) -e SHELL=${SHELL} --rm -t -i \
 	   --cap-add=SYS_ADMIN --cap-add=MKNOD --privileged \
 	   --device $(/sbin/losetup -f) \
 	   -e http_proxy=$http_proxy -e https_proxy=$https_proxy \
 	   -e no_proxy=$no_proxy \
 	   kasproject/kas-isar sh -c "
 		cd /out;
-		kas build /isar-jailhouse/kas.yml"
+		kas ${CMD} /jailhouse-images/kas.yml"
