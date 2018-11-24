siemens/jailhouse-images

commit e4db588a4d75630d1a138c7a03d536bef65011b2

jailhouse: Update to release 0.10
This allows to drop the no longer needed config files and all related
logic. All target images now work with this release.

With Jailhouse 0.10, the qemuarm64 target was grown to 16 CPUs, so
adjust our starter script accordingly.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-jailhouse/jailhouse/jailhouse.inc
@@ -9,24 +9,13 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 
-python() {
-    machine = d.getVar('MACHINE', True)
-    if machine in ['qemuamd64', 'nuc6cay']:
-        machine = 'x86-64'
-    # only work with --latest where configs became obsolete
-    if machine == 'espressobin':
-        machine = 'qemuarm64'
-    d.setVar('JAILHOUSE_CONFIG', machine + '_config.h')
-}
-
 DESCRIPTION = "Jailhouse partitioning hypervisor"
 
 LICENSE = "GPL-2.0 & BSD-2-clause"
 
 SRC_URI = " \
     git://github.com/siemens/jailhouse;branch=next \
-    file://debian/ \
-    file://${JAILHOUSE_CONFIG}"
+    file://debian/"
 
 S = "${WORKDIR}/git"
 
@@ -40,7 +29,6 @@ do_prepare_build() {
         # Install device trees only on non-x86 archs as they only exist there
         echo "configs{,/*}/dts/*.dtb etc/jailhouse/dts" >> ${S}/debian/jailhouse.install
     fi
-    cp ${WORKDIR}/${JAILHOUSE_CONFIG} ${S}/include/jailhouse/config.h
     sed -i 's/@PV@/${PV}/' ${S}/debian/changelog
     sed -i 's/@KERNEL_NAME@/${KERNEL_NAME}/' ${S}/debian/control
 }

conf/distro/jailhouse-demo.conf
@@ -15,4 +15,4 @@ IMAGE_PREINSTALL += "init"
 
 KERNEL_NAME = "jailhouse-${DISTRO_ARCH}"
 
-PREFERRED_VERSION_jailhouse ?= "0.9.1"
+PREFERRED_VERSION_jailhouse ?= "0.10"

images.list
@@ -1,5 +1,5 @@
 qemuamd64	QEMU/KVM Intel-x86 virtual target
 qemuarm64	QEMU ARM64 virtual target
 orangepi-zero	Orange Pi Zero (256 MB edition)
-nuc6cay		Intel NUC (NUC6CAY, 8 GB RAM)       [requires '--latest']
-espressobin	Marvell ESPRESSObin (1 GB edition)  [requires '--latest']
+nuc6cay		Intel NUC (NUC6CAY, 8 GB RAM)
+espressobin	Marvell ESPRESSObin (1 GB edition)

recipes-jailhouse/jailhouse/files/orangepi-zero_config.h
@@ -1,1 +1,0 @@
-#define CONFIG_MACH_ORANGEPI0		1

recipes-jailhouse/jailhouse/files/qemuarm64_config.h
@@ -1,1 +1,0 @@
-#define CONFIG_MACH_QEMU_ARM64		1

recipes-jailhouse/jailhouse/files/x86-64_config.h
@@ -1,1 +1,0 @@
-/* empty */

recipes-jailhouse/jailhouse/jailhouse_0.10.bb
@@ -11,4 +11,4 @@
 
 require jailhouse.inc
 
-SRCREV = "c2327d42887795a57163680a82b947fcb5e4a7d3"
+SRCREV = "f596aa7355bc2134650544bdf1e13f1f55d3f2fc"

start-qemu.sh
@@ -28,6 +28,7 @@ case "$1" in
 		QEMU=qemu-system-x86_64
 		QEMU_EXTRA_ARGS=" \
 			-cpu kvm64,-kvm_pv_eoi,-kvm_steal_time,-kvm_asyncpf,-kvmclock,+vmx,+arat \
+			-smp 4 \
 			-enable-kvm -machine q35,kernel_irqchip=split \
 			-serial vc \
 			-device ide-hd,drive=disk \
@@ -43,6 +44,7 @@ case "$1" in
 		QEMU=qemu-system-aarch64
 		QEMU_EXTRA_ARGS=" \
 			-cpu cortex-a57 \
+			-smp 16 \
 			-machine virt,gic-version=3,virtualization=on \
 			-device virtio-serial-device \
 			-device virtconsole,chardev=con -chardev vc,id=con \
@@ -70,6 +72,6 @@ shift 1
 
 ${QEMU_PATH}${QEMU} \
 	-drive file=${IMAGE_FILE},discard=unmap,if=none,id=disk,format=raw \
-	-m 1G -smp 4 -serial mon:stdio -netdev user,id=net \
+	-m 1G -serial mon:stdio -netdev user,id=net \
 	-kernel ${KERNEL_FILE} -append "${KERNEL_CMDLINE}" \
 	-initrd ${INITRD_FILE} ${QEMU_EXTRA_ARGS} "$@"

commit d54c3fb875a5c29425782eb2c08c9c85ea03f710

jailhouse: Install nuc6cay configs also for 0.10 release
These are no long latest-only.

Fixes: e4db588a4d75 ("jailhouse: Update to release 0.10")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-jailhouse/jailhouse/jailhouse.inc
@@ -15,7 +15,9 @@ LICENSE = "GPL-2.0 & BSD-2-clause"
 
 SRC_URI = " \
     git://github.com/siemens/jailhouse;branch=next \
-    file://debian/"
+    file://debian/ \
+    file://nuc6cay.c \
+    file://linux-nuc6cay-demo.c"
 
 S = "${WORKDIR}/git"
 
@@ -31,4 +33,7 @@ do_prepare_build() {
     fi
     sed -i 's/@PV@/${PV}/' ${S}/debian/changelog
     sed -i 's/@KERNEL_NAME@/${KERNEL_NAME}/' ${S}/debian/control
+
+    cp ${WORKDIR}/nuc6cay.c ${S}/configs/x86/
+    cp ${WORKDIR}/linux-nuc6cay-demo.c ${S}/configs/x86/
 }

recipes-jailhouse/jailhouse/jailhouse_latest.bb
@@ -14,13 +14,7 @@ require jailhouse.inc
 SRCREV = "next"
 PV = "0.9999-next"
 
-SRC_URI += " \
-    file://nuc6cay.c \
-    file://linux-nuc6cay-demo.c"
-
 dpkg_runbuild_prepend() {
     bbplain $(printf "jailhouse-latest: Building revision %.12s\n" \
                      $(cat ${S}/.git/refs/heads/next))
-    cp ${WORKDIR}/nuc6cay.c ${S}/configs/x86/
-    cp ${WORKDIR}/linux-nuc6cay-demo.c ${S}/configs/x86/
 }